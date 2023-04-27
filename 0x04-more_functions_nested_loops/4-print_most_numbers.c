a1374f9f53b186b68a976aef3f7461830424e146
Maroua <marouadnani@gmail.com>
2023-04-27 05:08:35 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,11 +13,8 @@ void print_most_numbers(void)
 	for (num = 0; num <= 9; num++)
 	{
 	if (num == 2 || num == 4)
-	continue;
-	}
-	else
-	{
-	_putchar(num + '0');
+		continue;
+	_putchar(num + 48);
 	}
 	_putchar('\n');
 }
