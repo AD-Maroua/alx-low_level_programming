989765050cbe4813ae4343284f1471b3cf4b7827
Maroua <marouadnani@gmail.com>
2023-05-24 05:53:29 -0700

    Task3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,61 +1,64 @@
 #include "3-calc.h"
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
- * op_add - adds two integers
- * @a: the first int
- * @b: the second int
- * Return: the sum
- **/
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The sum of a and b.
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
-
 /**
- * op_sub - returns the difference two integers
- * @a: the first int
- * @b: the second int
- * Return: the sum
- **/
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
- /**
-  * op_mul - returns the product two integers
-  * @a: the first int
-  * @b: the second int
-  * Return: the sum
- **/
-
+/**
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
-
- /**
-  * op_div - returns the result of the division two integers
-  * @a: the first int
-  * @b: the second int
-  * Return: the sum
-  **/
-
+/**
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The quotient of a and b.
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
-
- /**
-  * op_mod - returns the  remainder of the division two integers
-  * @a: the first int
-  * @b: the second int
-  * Return: the sum
- **/
+/**
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a by b.
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
 }
+
