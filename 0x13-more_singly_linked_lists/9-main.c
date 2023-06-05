8afbd7fc144cbf00f8a26e35c72f73a8ff17a505
maroua <marouadnani@gmail.com>
2023-06-05 22:50:46 +0100

    Task8

--- 0x13-more_singly_linked_lists/9-main.c
+++ 0x13-more_singly_linked_lists/9-main.c
@@ -5,7 +5,6 @@
 
 /**
  * main - check the code
- *
  * Return: Always 0.
  **/
 int main(void)
@@ -23,7 +22,7 @@ int main(void)
 	add_nodeint_end(&head, 1024);
 	print_listint(head);
 	printf("-----------------\n");
-	insert_nodeint_at_index(&head, 5, 4096);
+	insert_nodeint_at_index(&head, 5, 4096);		
 	print_listint(head);
 	free_listint2(&head);
 	return (0);
