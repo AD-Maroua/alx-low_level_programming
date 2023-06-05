2724688d5023a8a9aa2e3a914930b028d329c96a
maroua <marouadnani@gmail.com>
2023-06-05 20:08:11 +0100

    4-free_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+
 
 /**
  * struct listint_s - singly linked list
@@ -13,8 +15,8 @@
  **/
 typedef struct listint_s
 {
-		int n;
-			struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
